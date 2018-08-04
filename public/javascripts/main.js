dd2052db73358f5e046e64480511da7c1ba88d3c
Max van de Laar <max.vandelaar@me.com>
2018-08-04 15:35:42 +0200

    Further improved master layout and index page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,16 @@
+import * as Vibrant from 'node-vibrant';
+
 $(()=>{
+    $('[vibrant]').each((i, elem) => {
+        console.log(elem);
+        Vibrant.from(elem.getAttribute('src')).getPalette((err, palette) => {
+            const bgColor = palette.LightVibrant?palette.DarkVibrant.getHex():palette.DarkMuted.getHex();
+            const color = palette.LightVibrant?palette.LightVibrant.getHex():palette.LightMuted.getHex();
+
+            $(elem).closest('.card').css('background-color', bgColor);
+            $(elem).closest('.card').css('color', color);
+        })
+    });
+
     $(document).foundation();
 });
